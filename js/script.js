ddf139a9bbe5ba766e2dafae1ff3b52c26906b4c
sarah <skg@neofilm.us>
2022-05-15 18:05:27 -0400

    JS code to toggle light & dark mode, refactored

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
 // Change Instagram icon color
-var instagram = document.querySelector(".fa-instagram");
-instagram.style.color = "tomato";
+/*var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "tomato";*/
 
-// Add background image
-var foodImage = document.querySelector(".food");
-foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')";
+// Add background image 
+/*var foodImage = document.querySelector(".food");
+foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')"; */
 
-// Dotted border-bottom around h1
-var noms = document.querySelector("h1");
-noms.style.borderBottom = "4px dotted tomato";
+// Dotted border-bottom around h1 
+/*var noms = document.querySelector("h1");
+noms.style.borderBottom = "4px dotted tomato"; */
+
+/*
+var title = document.querySelector("h1");
+ 
+title.addEventListener("mouseover", function () { 
+     title.innerText = "Let's PARTY!";
+     title.style.color = "maroon";
+});
+
+var darkModeButton = document.querySelector(".dark-mode");
+var body = document.querySelector("body");
+ 
+darkModeButton.addEventListener("click", function () {
+ body.classList.add("dark-palette");
+});
+
+var lightModeButton = document.querySelector(".light-mode");
+ 
+lightModeButton.addEventListener("click", function () {
+ body.classList.remove("dark-palette");
+});
+*/
+
+// Toggle the styles on and off using just one button
+var darkModeButton = document.querySelector(".dark-mode");
+var theme = document.querySelector("body");
+ 
+ 
+darkModeButton.addEventListener("click", function () {
+ if (theme.classList.contains("dark-palette")) {
+   theme.classList.remove("dark-palette");
+   darkModeButton.innerText = "Dark Mode";
+ } else {
+   theme.classList.add("dark-palette");
+   darkModeButton.innerText = "Light Mode";
+ }
+});
